099ed5abc18e9afe6e3ddf3fa2d152d75a000f1f
Julian Mundhahs <Julian Mundhahs>
2016-06-11 11:58:20 +0200

    Authentifizieren gegenüber Mantis

--- javascript.js
+++ javascript.js
@@ -2,4 +2,25 @@ $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
     $('select').material_select();
-  });         
+  });
+
+function authenticate()
+{
+	var serverStamm = "https://stundenplan.hamburg.de/WebUntis/jsonrpc.do?school="
+	var schuleEndung = ""
+	var server = serverStamm + schuleEndung
+
+	$.ajax({
+		type: "POST",
+		url: server,
+		data: {
+			"id": "1",
+			"method": "authenticate",
+			"params": {
+				"user": document.getElementById("username").textContent,
+				"password": document.getElementById("password").textContent
+			},
+			"jsonrpc": "2.0"
+		}
+	});
+}
