415864deb1da9e4117897b696ba0be6a6e041098
Julian Mundhahs <Julian Mundhahs>
2016-06-18 15:36:01 +0200

    Python Server und node_modules entfernt

--- discipulus/js/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $("#btn").click(function() {
-        $("#paragraph").html("Hallo") + ;
-    });
-})
-     /*document.getElementById("paragraph").innerHTML = "Hallo";*/
